efddad48b3770d1ab23a3944d35e5d71dc805c1d
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-07-24 23:00:13 -0700

    Set up spring boot project

---
+++ Game-API/src/test/java/com/example/Game/API/GameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Game.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
